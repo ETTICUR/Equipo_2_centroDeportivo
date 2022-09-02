a7de554676946520f0d52fcbc0e0a31c5d78e6a0
MateoAscona <asconamateo@gmail.com>
2022-09-02 19:47:59 -0300

    funcion borrar y recuperar productos

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,8 @@
+const fs = require("fs")
+const path = require("path")
 let actividades = require("../data/actividades.json")
+let actividadesBorradas = require("../data/actividadesBorradas.json")
+
 
 let controller = {
    cart: (req, res) => {
@@ -32,6 +36,54 @@ let controller = {
       title: 'Editar Actividad',
       actividad: actividadSeleccionada
     });
+  },
+
+  delete: (req, res) => {
+    let idSeleccionado = Number(req.params.id)
+    
+    let data = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/actividades.json")))
+    let eliminadas = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/actividadesBorradas.json")))
+
+    let actividadesRestantes = data.filter(e => e.id !== idSeleccionado )
+    let actividadEliminada = data.find(e => e.id == idSeleccionado )
+
+    eliminadas.push(actividadEliminada)
+    
+    let actividadesRestantesJSON = JSON.stringify(actividadesRestantes)
+    let actividadEliminadaJSON = JSON.stringify(eliminadas)
+
+    fs.writeFileSync(path.join(__dirname, "../data/actividades.json"), actividadesRestantesJSON)
+    fs.writeFileSync(path.join(__dirname, "../data/actividadesBorradas.json"), actividadEliminadaJSON)
+
+    res.redirect("/")
+  },
+
+  deletedProducts: (req, res) => {
+
+    res.render('productDeleted', {
+      title: 'Actividades Borradas',
+      actividadesBorradas
+    });
+  },
+
+  productRecovery: (req, res) => {
+    let idSeleccionado = Number(req.params.id)
+
+    let data = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/actividades.json")))
+    let eliminadas = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/actividadesBorradas.json")))
+
+    let recuperarActividad = eliminadas.find(e => e.id == idSeleccionado )
+    let actividadesBorradasRestantes = eliminadas.filter(e => e.id !== idSeleccionado )
+
+    data.push(recuperarActividad)
+
+    let recuperarActividadJSON = JSON.stringify(data)
+    let actividadesBorradasRestantesJSON = JSON.stringify(actividadesBorradasRestantes)
+
+    fs.writeFileSync(path.join(__dirname, "../data/actividades.json"), recuperarActividadJSON)
+    fs.writeFileSync(path.join(__dirname, "../data/actividadesBorradas.json"), actividadesBorradasRestantesJSON)
+
+    res.redirect("/")
   }
 
 }
--- router/productRoutes.js
+++ router/productRoutes.js
@@ -5,6 +5,10 @@ const productController = require('../controllers/productController');
 router.get("/carrito", productController.cart);
 
 router.get("/detalle/:id", productController.detail);
+router.post("/detalle/:id", productController.delete)
+
+router.get("/recuperar", productController.deletedProducts)
+router.post("/recuperar/:id", productController.productRecovery)
 
 router.get("/crear", productController.create);
 router.post("/crear", productController.processCreate);
