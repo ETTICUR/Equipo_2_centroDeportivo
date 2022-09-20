1f30262ee43ab133e6e7b371daa7d007d2659589
MariaLauraFlaxMarco <todosepuede_victoria@yahoo.com.ar>
2022-09-20 19:31:24 -0300

    Actualizacion de process register

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const {validationResult}= require('express-validator');
+const bcryptjs = require("bcryptjs");
 
 let controller = {
     login: (req, res) => {
@@ -22,7 +23,56 @@ let controller = {
                 oldData: req.body
             })
         }else{
-            res.send('Usuario Registrado')
+            let usuariosObjeto = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/user.json")));
+            let corroborarUsuario = usuariosObjeto.find(usuarioActual => usuarioActual.email == req.body.email);
+
+            if(!corroborarUsuario) {
+                let nuevoUsuario = {
+                    id: usuariosObjeto.length + 1,
+                    nombre: req.body.nombre,
+                    apellido: req.body.apellido,
+                    genero: req.body.genero,
+                    edad: req.body.edad,
+                    actividad: req.body.actividad,
+                    email: req.body.email,
+                    password: bcryptjs.hashSync(req.body.password, 10),
+                    passwordConfirm: bcryptjs.hashSync(req.body.passwordConfirm, 10),
+                    condiciones: req.body.condiciones,
+                    fotoPerfil: "/images/users" + req.file.filename
+                };
+
+                if(bcryptjs.compareSync(req.body.password, nuevoUsuario.passwordConfirm)) {
+                    usuariosObjeto.push(nuevoUsuario);
+
+                    let usuariosObjetoJSON = JSON.stringify(usuariosObjeto, null, " ");
+
+                    fs.writeFileSync(path.join(__dirname, "../data/user.json"), usuariosObjetoJSON);
+
+                    res.redirect("login");
+
+                }else{
+                    res.render("register", {
+                        title: "Registro", 
+                        errors: {
+                            passwordConfirm:{
+                                msg: "La contraseña ingresada no coincide"
+                            }
+                        },
+                        oldData: req.body
+                    })
+                }
+            
+            } else{
+                res.render("register", {
+                    title: "Registro", 
+                    errors: {
+                        email:{
+                            msg: "Este mail ya se encuentra registrado"
+                        }
+                    },
+                    oldData: req.body
+                })
+            }   
         }
     },
 
